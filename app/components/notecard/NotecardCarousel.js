25c84f5a0df24a8ce7965f0b4733e48abd0ef3ee
anniesteenson <anniesteenso@gmail.com>
2017-01-22 21:56:52 -0600

    Playing around with backgrounds. Setting up Carousel with Linked List

--- app/components/notecard/NotecardCarousel.js
+++ app/components/notecard/NotecardCarousel.js
@@ -9,29 +9,40 @@ export default class NotecardCarousel extends React.Component {
     };
 
     state = {
-        foo: "bar"
+        nodecards: "bar"
     };
 
     constructor(props) {
         super(props);
+
+        this.rotateRight = this.rotateRight.bind(this);
+        this.rotateLeft = this.rotateLeft.bind(this);
+    }
+
+    rotateRight() {
+
+    }
+
+    rotateLeft() {
+
     }
 
     render() {
         return (
         <div className="modal fade" id="addCardModal" tabIndex="-1" role="dialog">
-            <div className="notecard-carousel carousel-left">
+            <div className="notecard-carousel carousel-left" onClick={this.rotateRight}>
                 <CarouselItem viewId={0}/>
             </div>
 
-            <div className="notecard-carousel carousel-left-arrow">
-                <CarouselItem viewId={2}/>
+            <div className="notecard-carousel carousel-left-arrow" onClick={this.rotateRight}>
+                <CarouselItem viewId={4}/>
             </div>
 
-            <div className="notecard-carousel carousel-right-arrow">
+            <div className="notecard-carousel carousel-right-arrow" onClick={this.rotateLeft}>
                 <CarouselItem viewId={3}/>
             </div>
 
-            <div className="notecard-carousel carousel-right">
+            <div className="notecard-carousel carousel-right" onClick={this.rotateLeft}>
                 <CarouselItem viewId={1}/>
             </div>
 
