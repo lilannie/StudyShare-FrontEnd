90789266fef0eeaada017c9c0e95e59c5a650b53
anniesteenson <anniesteenso@gmail.com>
2017-01-27 22:35:27 -0600

    Added a link tag to the search bar shortcut

--- app/components/common/header/HeaderSearch.js
+++ app/components/common/header/HeaderSearch.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {Link} from 'react-router';
+
 export default class HeaderSearch extends React.Component {
     static defaultProps = {
         foo: 'bar'
@@ -19,10 +21,12 @@ export default class HeaderSearch extends React.Component {
                 <div className="input-group">
                     <input type="text" className="form-control" placeholder="Search"/>
                     <span className="input-group-btn">
+                        <Link to={'/search'}>
                         <button className="btn btn-default" type="submit">
                             <span className="glyphicon glyphicon-search" aria-hidden="true">
                             </span>
                         </button>
+                        </Link>
                     </span>
                 </div>
             </form>
